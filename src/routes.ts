57565f37be74a80dbd8f16ec95f30141d7bb2ffb
Camila <salescamila1@gmail.com>
2020-06-21 21:23:03 -0400

    Utilizando interface

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,10 @@ import { Request, Response } from 'express';
 import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response){
-  const user = createUser('Camila', 'camilas.dev@gmail.com', '123456');
+  const user = createUser({
+    email: 'camilas.dev@gmail.com',
+    password: '123456'
+  });
 
   return response.json({ message: 'Hello World' });
 }
--- src/services/CreateUser.ts
+++ src/services/CreateUser.ts
@@ -2,7 +2,13 @@
  * Para criar: name, email, password
  */
 
-export default function createUser(name = '', email: string, password: string) {
+interface CreateUserData {
+  name ?: string;
+  email : string;
+  password : string;
+}
+
+export default function createUser({ name, email, password }: CreateUserData) {
    const user = {
      name,
      email,

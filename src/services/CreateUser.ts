6a9b49c4cf64afa1f6d7b85b2d4f64c28c09c587
Camila <salescamila1@gmail.com>
2020-06-21 21:35:25 -0400

    Personalizando a tipagem

--- src/services/CreateUser.ts
+++ src/services/CreateUser.ts
@@ -2,10 +2,17 @@
  * Para criar: name, email, password
  */
 
+interface TechObject {
+  title: string;
+  experience: number;
+}
+
 interface CreateUserData {
   name ?: string;
-  email : string;
+  email : string ;
   password : string;
+  techs : Array<string | TechObject>;
+  phone ?: string[];
 }
 
 export default function createUser({ name, email, password }: CreateUserData) {
